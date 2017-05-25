72184674b366c81691e2d468ea7c559cdc3ba49a
Nightang Tang <nightang@gmail.com>
2017-05-25 19:41:01 +0800

    Fix configuration

--- stock-price-finder-core/src/main/java/org/nightang/stock/listfinder/HKEXListFinder.java
+++ stock-price-finder-core/src/main/java/org/nightang/stock/listfinder/HKEXListFinder.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -30,50 +32,68 @@ public class HKEXListFinder {
 	}
 	
 	public List<StockInfo> findStockList() throws IOException {
-		List<StockInfo> list = new ArrayList<StockInfo>();
-
+		Map<String, StockInfo> map = new HashMap<String, StockInfo>();
+		
+		buildStockList(map);
+		updateChiName(map);
+		
+		return null;
+	}
+	
+	private void buildStockList(Map<String, StockInfo> map) throws IOException {
 		String url = "http://www.hkex.com.hk/eng/market/sec_tradinfo/stockcode/eisdeqty.htm";
 		String data = hw.doGet(url);
 		//log.info("RAW: " + data);
-
 		Date now = new Date();
-		try {
-			
-			/*
-			 * 	Sample:
-				<tr class="tr_normal">
-                	<td class="verd_black12" width="18%">03813</td>
-                	<td class="verd_black12" width="42%"><a href="../../../invest/company/profile_page_e.asp?WidCoID=03813&WidCoAbbName=&Month=&langcode=e" target="_parent">POU SHENG INT'L</a></td>
-                	<td class="verd_black12" width="19%">1,000</td>
-                	<td class="verd_black12" width="3%" align="center">#</td>
-                	<td class="verd_black12" width="3%">H</td>
-                	<td class="verd_black12" width="3%">O</td>
-                	<td class="verd_black12" width="3%">&nbsp;</td>
-                </tr>
-			 */
-			String pStr = "<td[^>]*>" + "(\\d{5})" + "</td>\\s*"
-						+ "<td[^>]*><a[^>]*>" + "([^<]*)" + "</a></td>\\s*"
-						+ "<td[^>]*>" + "([^<]*)" + "</td>\\s*";
-			Pattern p = Pattern.compile(pStr);
-			Matcher m = p.matcher(data);
-			
-			while (m.find()) {
-				MatchResult mr = m.toMatchResult();				
-				StockInfo stock = new StockInfo();
-				stock.setStockNum(mr.group(1));
-				stock.setEngName(mr.group(2));
-				stock.setBoardLot(Integer.parseInt(mr.group(3).replace(",", "")));
-				stock.setActive(true);
-				stock.setLastModifiedDate(now);
-				list.add(stock);
-			}
-			log.info("Number of Stocks: " + list.size());
-			
-		} catch (RuntimeException re) {
-			log.error("ErrMsg: " + re);
-		}
+		/*
+		 * 	Sample:
+			<tr class="tr_normal">
+               	<td class="verd_black12" width="18%">03813</td>
+               	<td class="verd_black12" width="42%"><a href="../../../invest/company/profile_page_e.asp?WidCoID=03813&WidCoAbbName=&Month=&langcode=e" target="_parent">POU SHENG INT'L</a></td>
+               	<td class="verd_black12" width="19%">1,000</td>
+               	<td class="verd_black12" width="3%" align="center">#</td>
+               	<td class="verd_black12" width="3%">H</td>
+               	<td class="verd_black12" width="3%">O</td>
+               	<td class="verd_black12" width="3%">&nbsp;</td>
+               </tr>
+		 */
+		String pStr = "<td[^>]*>" + "(\\d{5})" + "</td>\\s*"
+					+ "<td[^>]*><a[^>]*>" + "([^<]*)" + "</a></td>\\s*"
+					+ "<td[^>]*>" + "([^<]*)" + "</td>\\s*";
+		Pattern p = Pattern.compile(pStr);
+		Matcher m = p.matcher(data);
 		
-		return list;
+		while (m.find()) {
+			MatchResult mr = m.toMatchResult();				
+			StockInfo stock = new StockInfo();
+			stock.setStockNum(mr.group(1));
+			stock.setEngName(mr.group(2));
+			stock.setBoardLot(Integer.parseInt(mr.group(3).replace(",", "")));
+			stock.setActive(true);
+			stock.setLastModifiedDate(now);
+			map.put(stock.getStockNum(), stock);
+		}
+		log.info("Number of Stocks: " + map.size());			
+	}
+	
+	private void updateChiName(Map<String, StockInfo> map) throws IOException {
+		String url = "http://www.hkex.com.hk/eng/market/sec_tradinfo/stockcode/eisdeqty_c.htm";
+		String data = hw.doGet(url);
+		// Same as "buildStockList"
+		String pStr = "<td[^>]*>" + "(\\d{5})" + "</td>\\s*"
+				+ "<td[^>]*><a[^>]*>" + "([^<]*)" + "</a></td>\\s*"
+				+ "<td[^>]*>" + "([^<]*)" + "</td>\\s*";
+		Pattern p = Pattern.compile(pStr);
+		Matcher m = p.matcher(data);
+		while (m.find()) {
+			MatchResult mr = m.toMatchResult();	
+			String stockNum = mr.group(1);
+			String chiName = mr.group(2);
+			StockInfo stock = map.get(stockNum);
+			if(stock != null) {
+				//stock.setEngName(engName);
+			}
+		}		
 	}
 	
 }
