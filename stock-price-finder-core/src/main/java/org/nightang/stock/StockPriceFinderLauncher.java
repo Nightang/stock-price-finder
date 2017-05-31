538efa33f5280ad63bc8289b821abd553d599a2e
Nightang Tang <nightang@gmail.com>
2017-06-01 02:58:48 +0800

    Update

--- stock-price-finder-core/src/main/java/org/nightang/stock/StockPriceFinderLauncher.java
+++ stock-price-finder-core/src/main/java/org/nightang/stock/StockPriceFinderLauncher.java
@@ -1,17 +1,33 @@
 package org.nightang.stock;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 import org.nightang.stock.service.StockListService;
+import org.nightang.stock.service.StockPriceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
 
+@Service
 public class StockPriceFinderLauncher {
 
-	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("context-application.xml");
-		StockListService spfl = (StockListService) context.getBean("StockListService");
-		
+	@Autowired
+	private StockListService stockListService;
 
+	@Autowired
+	private StockPriceService stockPriceService;
+	
+	public void doTask() throws IOException, GeneralSecurityException {
+		stockListService.updateStockList();		
+		stockPriceService.updateStockPriceData();		
+	}
+
+	public static void main(String[] args) throws IOException, GeneralSecurityException {
+		ApplicationContext context = (ApplicationContext) new ClassPathXmlApplicationContext("context-application.xml");
+		StockPriceFinderLauncher launcher = (StockPriceFinderLauncher) context.getBean("stockPriceFinderLauncher");
+		launcher.doTask();		
 	}
 
 }
